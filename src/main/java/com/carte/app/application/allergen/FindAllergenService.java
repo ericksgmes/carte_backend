bfcaa12bc369fdfc17187cb9a29529c5c4b3e553
Erick Santinon Gomes <erick.dicksan@gmail.com>
2025-11-18 17:44:23 -0300

    feat: create find all allergens service

---
+++ src/main/java/com/carte/app/application/allergen/FindAllergenService.java
@@ -0,0 +1,25 @@
+package com.carte.app.application.allergen;
+
+import com.carte.app.infrastructure.persistence.entity.AllergenEntity;
+import com.carte.app.infrastructure.persistence.mapper.AllergenMapper;
+import com.carte.app.infrastructure.persistence.repository.AllergenDto;
+import com.carte.app.infrastructure.persistence.repository.JpaAllergenRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class FindAllergenService{
+    private final JpaAllergenRepository jpaAllergenRepository;
+    public FindAllergenService(
+            JpaAllergenRepository repository
+    ) {
+        this.jpaAllergenRepository = repository;
+    }
+
+    List<AllergenDto> findAll() {
+        List<AllergenEntity> allergens = jpaAllergenRepository.findAll();
+        return allergens.stream().map(AllergenMapper::toAllergenDto).collect(Collectors.toList());
+    }
+}
